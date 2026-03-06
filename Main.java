8256bf2a6dd6caa72dc63ce2bdc8ac8a7ef9aa15
ciomperlik-zac <zciomperlik@gmail.com>
2026-03-06 17:31:16 -0600

    DONE

--- Main.java
+++ Main.java
@@ -1,15 +1,15 @@
 public class Main {
     public static void main(String[] args) {
-        int[] array = {29, 10, 14, 37, 13};
+        int[] array = { 29, 10, 14, 37, 13 };
         System.out.println("Original Array: ");
         printArray(array);
-        
+
         selectionSort(array);
-        
+
         System.out.println("Sorted Array: ");
         printArray(array);
     }
-    
+
     public static void printArray(int[] array) {
         for (int num : array) {
             System.out.print(num + " ");
@@ -18,6 +18,21 @@ public class Main {
     }
 
     public static void selectionSort(int[] array) {
-        
+        int start = 0;
+
+        for (int i = 0; i < array.length; i++) {
+            int minIndex = start;
+
+            for (int i_ = start; i_ < array.length; i_++) {
+                if (array[i_] < array[minIndex])
+                    minIndex = i_;
+            }
+
+            int temp = array[start];
+            array[start] = array[minIndex];
+            array[minIndex] = temp;
+
+            start++;
+        }
     }
 }
